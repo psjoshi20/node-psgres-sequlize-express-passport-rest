0f33433bd9a08a1b7fbd0cb8f1ed94016487afbd
psjoshi20 <psjoshi20@gmail.com>
2019-12-16 11:12:17 +0800

    db:seed not resolved

--- models/user.js
+++ models/user.js
@@ -1,12 +1,36 @@
-'use strict';
+const bcrypt= require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    userName: DataTypes.STRING,
-    passwordHash: DataTypes.STRING,
-    userRoleId: DataTypes.INTEGER
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
   }, {});
+  User.beforeSave((user, options) => {
+    if (user.changed('password')) {
+      user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    }
+  });
+  User.prototype.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+  };
   User.associate = function(models) {
     // associations can be defined here
   };
   return User;
-};
+};// 'use strict';
+// module.exports = (sequelize, DataTypes) => {
+//   const User = sequelize.define('User', {
+//     userName: DataTypes.STRING,
+//     passwordHash: DataTypes.STRING,
+//     userRoleId: DataTypes.INTEGER
+//   }, {});
+//   User.associate = function(models) {
+//     // associations can be defined here
+//   };
+//   return User;
+// };
